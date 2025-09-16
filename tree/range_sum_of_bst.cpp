af33e8440f62194ec894461e92028f49d7bef012
Neeraj Kumar <thakurneerajkumar17@gmail.com>
2025-09-16 13:52:09 +0530

    trees 2 more questions

---
+++ tree/range_sum_of_bst.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    void rsum(TreeNode* root, int low, int high,int & sum){
+        if(root==NULL) return;
+        if (root->val >= low && root->val <= high){
+            sum+=root->val;
+        }
+        if(root->val>=low){
+            rsum(root->left,low,high,sum);
+        }
+        if(root->val<=high){
+            rsum(root->right,low,high,sum);
+        }
+        return;
+
+    }
+
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        int sum=0;
+        rsum(root,low,high,sum);
+        return sum;
+    }
+};
