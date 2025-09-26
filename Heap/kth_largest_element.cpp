6faadaba7579bc8fa8357605b43c69305ed3c4d8
Neeraj Kumar <thakurneerajkumar17@gmail.com>
2025-09-26 18:48:47 +0530

    kth largest element

---
+++ Heap/kth_largest_element.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int,vector<int>,greater<int>> minheap;
+        int n=nums.size();
+        
+        for(int i=0; i<n; i++){
+            minheap.push(nums[i]);
+            if(minheap.size()>k){
+                minheap.pop();
+            }
+        }
+        int ans=minheap.top();
+        return ans;
+    }
+};
