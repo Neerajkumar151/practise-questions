74c18f600a4ac3d81533ee39c27878694c8b54a9
Neeraj Kumar <thakurneerajkumar17@gmail.com>
2025-10-07 21:49:24 +0530

    rotate string

---
+++ string/rotate_string.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        // int n=s.size();
+        // for(int i=1; i<=n; i++){
+        //     rotate(s.begin(),s.begin()+1,s.end());
+        //     if(s==goal) return true;
+        // }
+        // return false;
+
+        // string temp=s+s;
+        // if(s.size()!=goal.size()) return false;
+        // if(temp.find(goal)!=string::npos){
+        //     return true;
+        // }
+        // return false;
+    }
+};
