fc8a2d96951fa7fe12b14acb2dbced39be90960e
NEERAJ KUMAR <thakurneerajkumar17@gmail.com>
2026-02-03 12:59:46 +0530

    169 leetcode

--- array/majority_element.cpp
+++ array/majority_element.cpp
@@ -1,64 +1,23 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        // unordered_map<int,int> frequency;
-
-        // for(int i=0; i<nums.size(); i++){
-        //     frequency[nums[i]]++;
-        //     if(frequency[nums[i]]>(nums.size()/2)){
-        //         return nums[i];
-        //     }
-        // }
-        // return -1;
-
-
-        // sort(nums.begin(), nums.end());
-        // return nums[nums.size() / 2];
-
-        //moore's voting algorithm
+        int n=nums.size();
         
-        // int ans=0;
-        // int element=0;
-        // int count=0;
-        // for(int i=0; i<nums.size(); i++){
-        //     if(count==0){
-        //         element=nums[i];
-        //     }
-        //     if(nums[i]==element){
-        //         count++;
-        //         ans=nums[i];
-        //     }
-        //     else {
-        //         count--;
-        //     }
-        // }
-        // return ans;
-
-
-
-        int element=0;
+        int candidate=nums[0];
         int count=0;
-        for(int i=0; i<nums.size(); i++){
-            if(count==0){
-                element=nums[i];
-            }
-            if(nums[i]==element){
+        for(int i=0; i<n; i++){
+            if(candidate==nums[i]){
                 count++;
             }
             else {
                 count--;
+                if(count==0){
+                    candidate=nums[i];
+                    count++;
+                }
             }
         }
-        count=0;
+        return candidate;
 
-        for(int i=0; i<nums.size(); i++){
-            if(nums[i]==element){
-                count++;
-            }
-            if(count>nums.size()/2){
-                return nums[i];
-            }
-        }
-        return -1;
     }
 };
