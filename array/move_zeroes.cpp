bb6eb117b03bd4aa87a6560664f6324850ac27ee
NEERAJ KUMAR <thakurneerajkumar17@gmail.com>
2026-02-09 11:25:15 +0530

    leetcode 283

--- array/move_zeroes.cpp
+++ array/move_zeroes.cpp
@@ -1,32 +1,16 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        // int i=0; int j=1;
-        // if(nums.size()==1) return;
-        // while(j<nums.size()){
-        //     if(nums[i]==0 && nums[j]!=0){
-        //         swap(nums[i],nums[j]);
-        //         i++;
-        //         j++;
-        //     }
-        //     else if(nums[i]==0 && nums[j]==0){
-        //         j++;
-        //     }
-        //     else {
-        //         i++;
-        //         j++;
-        //     }
-            
-        // }
+        int n=nums.size();
+        if(n==1) return;
+        int k=0;
 
-
-        int nonzeroindex=0;
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] != 0) {
-                swap(nums[i], nums[nonzeroindex]);
-                nonzeroindex++;
+        for(int i=0; i<n; i++){
+            if(nums[i]!=0){
+                swap(nums[k],nums[i]);
+                k++;
             }
         }
-
+        return;
     }
 };
