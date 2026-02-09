ed0b71683699595cfe3a58d22d61883340b31863
NEERAJ KUMAR <thakurneerajkumar17@gmail.com>
2026-02-09 12:28:36 +0530

    leetcode 189

--- array/rotate_array.cpp
+++ array/rotate_array.cpp
@@ -1,29 +1,15 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        if(k==0 || k==nums.size()){
-            return;
-        }
-        // vector<int> ans;
-        // int n=nums.size();
-        // k=k%n;
-        // for(int i=n-k; i<n; i++){
-        //     ans.push_back(nums[i]);
-        // }
-        // for(int i=0; i<n-k; i++){
-        //     ans.push_back(nums[i]);
-        // }
-        // for(int i=0; i<n; i++){
-        //     nums[i]=ans[i];
-        // }
-        // return;
+        int n = nums.size();
+        if (n <= 1) return;
 
-        int n=nums.size();
-        k=k%n;
-        reverse(nums.begin(),nums.end());
-        reverse(nums.begin(),nums.begin()+k);
-        reverse(nums.begin()+k,nums.end());
-        return;
+        k %= n;
+        if (k == 0) return;
 
+        int temp = n - k;
+        reverse(nums.begin(), nums.begin() + temp);
+        reverse(nums.begin() + temp, nums.end());
+        reverse(nums.begin(), nums.end());
     }
 };
