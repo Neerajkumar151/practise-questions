bbc389b0760ca06d79ef51c5bf7cf05c6b7cdb1d
NEERAJ KUMAR <thakurneerajkumar17@gmail.com>
2026-02-21 11:48:33 +0530

    leetcode question

---
+++ array/continuous_subarray_sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        int n=nums.size();
+
+        unordered_map<int,int> map;
+
+        int sum=0;
+        map[0]=-1;
+        for(int i=0; i<n; i++){
+            sum+=nums[i];
+            int remainder=sum%k;
+            if(map.find(remainder)!=map.end()){
+                if(i- map[remainder] >= 2) return true;
+            }
+            else {
+                map[remainder]=i;
+            }
+            
+        }
+        return false;
+    }
+};
